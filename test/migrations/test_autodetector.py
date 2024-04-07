e72dab2dfa91f58dd29ae94f3c55aac8fbb088dc
Geflic <sslicc6@gmail.com>
2024-04-07 22:19:32 +0200

    lets try again

--- test/migrations/test_autodetector.py
+++ test/migrations/test_autodetector.py
@@ -21,15 +21,15 @@ def test_rename_field_preserved_db_column():
   changes = get_changes(Questioner({'ask_rename': True}))
   assertNumberMigrations(changes, 'app', 1)
   assertOperationTypes(
-    changes, 'app', 0, ['RenameField', 'AlterField']
+    changes, 'app', 0, ['AlterField', 'RenameField']
   )
   assertOperationAttributes(
-    changes, 'app', 0, model_name='foo', old_name='field',
-    new_name='renamed_field',
+    changes, 'app', 0, 0,
+    model_name='foo', name='field'
   )
   assertOperationAttributes(
-    changes, 'app', 0, 1, model_name='foo',
-    name='renamed_field'
+    changes, 'app', 1, model_name='foo',
+    old_name='field', new_name='renamed_field',
   )
 
 def test_rename_related_field_preserved_db_column(self):
